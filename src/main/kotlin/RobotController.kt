eed9a02b11666e4e847b26a881179f15c013092a
Tjark Seemann <tjarkseemann@macbookpro.speedport.ip>
2025-01-01 17:05:21 +0100

    basic controll functions for the roboter

--- src/main/kotlin/RobotController.kt
+++ src/main/kotlin/RobotController.kt
@@ -5,7 +5,7 @@ import de.fhkiel.rob.legoosctester.osc.OSCSender
 import de.fhkiel.rob.legoosctester.osc.OSCReceiver
 
 enum class RoboterControls{
-    LEFTTURN,RIGHTTURN,FOWARD,TURNEYESRIGHT,TURNEYESLEFT,TURN180DEGREES,NONE
+    LEFTTURN,RIGHTTURN,FORWARD,TURNEYESRIGHT,TURNEYESLEFT,TURN180DEGREES,AGAINSTWALL,NONE
 }
 
 class RobotController {
@@ -17,6 +17,19 @@ class RobotController {
         OSCReceiver.addListener { path, args ->
             onMessageReceived(path, args)
         }
+    }
+
+    fun driveIntoNextCell(){
+        currentControl = RoboterControls.FORWARD
+        OSCSender(robotState.robotIp, robotState.robotPort).send("/192.168.178.152/motor/bc/angle", 0)
+        OSCSender(robotState.robotIp, robotState.robotPort).send("/192.168.178.152/motor/bc/multirun/target", 100, 140, 140)
+    }
+
+    fun headbuttWall(){
+        currentControl = RoboterControls.AGAINSTWALL
+        OSCSender(robotState.robotIp, robotState.robotPort).send("/192.168.178.152/motor/bc/angle", 0)
+        OSCSender(robotState.robotIp, robotState.robotPort).send("/192.168.178.152/motor/bc/multirun/target", 100, 140, 140)
+
     }
 
     fun turnLeft90Degree(){
@@ -33,6 +46,15 @@ class RobotController {
         OSCSender(robotState.robotIp, robotState.robotPort).send("/192.168.178.152/motor/bc/multirun/target", 100, -140, 140)
 
     }
+
+    fun turn180Degree(){
+        currentControl = RoboterControls.TURN180DEGREES
+        OSCSender(robotState.robotIp, robotState.robotPort).send("/192.168.178.152/gyroscope/s3/angle", 0)
+        OSCSender(robotState.robotIp, robotState.robotPort).send("/192.168.178.152/motor/bc/angle", 0)
+        OSCSender(robotState.robotIp, robotState.robotPort).send("/192.168.178.152/motor/bc/multirun/target", 100, -280, 280)
+
+    }
+
 
     private fun onMessageReceived(path: String, args: List<Any>) {
         if (path == "/OSCBrick@192.168.178.152/gyroscope/s3/angle/is" && args[0] != 0) {
